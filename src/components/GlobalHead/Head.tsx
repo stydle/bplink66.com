b11c223a7d23ebed01183b8441cb12397cd2bf39
stydle <bctnab66@163.com>
2023-02-06 09:29:23 +0800

    update favicons icon

--- src/components/GlobalHead/Head.tsx
+++ src/components/GlobalHead/Head.tsx
@@ -3,10 +3,12 @@ import NextHead from 'next/head'
 import { useRouter } from 'next/router'
 import Config from '@/config/config'
 
+const GENERIC_TITLE = `${Config.name}'s portal site`;
+
 const GENERIC_DESCRIPTION =
   'Friendly tutorials for developers. Focus on React, CSS, Animation, and more!'
 
-const PROD_URL = 'https://www.joshwcomeau.com'
+const PROD_URL = 'https://www.bplink66.com'
 interface MetaProps {
   title?: string
   ogImage?: string
@@ -17,38 +19,36 @@ function Head({ description = '', title, ogImage }: MetaProps) {
   const router = useRouter()
 
   const meta = {
-    title: title || Config.name,
+    title: title || GENERIC_TITLE,
     description: description || GENERIC_DESCRIPTION,
-    image: PROD_URL + (ogImage || '/favicons/og-default.jpg'),
+    image: PROD_URL + (ogImage || '/favicons/og-default.png'),
     type: 'website',
+    imgWidth: 1280,
+    imgHeight: 640
   }
 
   return (
     <NextHead>
       <title>{meta.title}</title>
-      <meta charSet="utf-8" />
-      <meta name="description" content={meta.description} />
 
-      {/* TODO: Are these necessary? */}
-      <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+      <meta charSet="utf-8" />
       <meta
         name="viewport"
         content="width=device-width, initial-scale=1, shrink-to-fit=no"
       />
-
-      <meta property="og:url" content={`${PROD_URL}${router.asPath}`} />
-      <meta property="og:type" content={meta.type} />
-      <meta property="og:description" content={meta.description} />
-      <meta property="og:title" content={meta.title} />
-      <meta property="og:image" content={meta.image} />
-      <meta property="og:image:width" content="1280" />
-      <meta property="og:image:height" content="675" />
-
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:title" content={meta.title} />
-      <meta name="twitter:description" content={meta.description} />
-      <meta name="twitter:image" content={meta.image} />
+      <meta name="description" content={meta.description} />
+      <meta httpEquiv="x-ua-compatible" content="ie=edge" />
       <meta name="theme-color" content="hsl(204deg, 67%, 85%)" />
+
+      {/* og-card */}
+      <meta property="og:url" content={`${PROD_URL}${router.asPath}`} />
+      <meta property="og:title" content={meta.title} />
+      <meta property="og:site_name" content={title} />
+      <meta property="og:description" content={meta.description} />
+      <meta property="og:type" content="website" />
+      <meta property="og:image" content={meta.image} />
+      <meta property="og:image:width" content={`${meta.imgWidth}`} />
+      <meta property="og:image:height" content={`${meta.imgHeight}`} />
 
       <link rel="shortcut icon" href="/favicons/favicon.ico" />
       <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
